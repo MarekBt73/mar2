06cdb7e02ae34956791c749c89e7926170920127
Marek Becht <marek.becht@gmail.com>
2021-08-23 22:08:43 +0200

    start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Blog Marka`,
+    description: `Jeżeli chcesz się czegoś nauczyć powinieneś ćwiczyć - ćwiczyć i jeszcze raz ćwiczyć!`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
