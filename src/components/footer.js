dfd2027d7acbf9b74f87f55105e9914a3f1482ae
Marek Becht <marek.becht@gmail.com>
2021-08-23 22:51:37 +0200

    dodany footer 1

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const Footer = () => (
+  <footer
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/" style={{ color: `white`, textDecoration: `none`, }}
+                                     > 
+                 <h4>© Wszystkie prawa zastrzeżone</h4>
+
+        </Link>
+      </h1>
+    </div>
+  </footer>
+)
+
+export default Footer
+
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -32,19 +25,12 @@ const Layout = ({ children }) => {
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+           >
+              <main>{children}</main>
+       
+            </div>
+        <Footer/>
+     </>
   )
 }
 
